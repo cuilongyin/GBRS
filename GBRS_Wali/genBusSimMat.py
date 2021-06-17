29a4549d33521c3f1e08966326ed7f4c4e68d10b
cuilongyin <cuilongyin@hotmail.com>
2021-06-17 03:01:36 -0400

    Currently Stucked of how to write to a simFile

--- GBRS_Wali/genBusSimMat.py
+++ GBRS_Wali/genBusSimMat.py
@@ -1,18 +1,95 @@
-import pandas as pd
+import csv
+import sys
 import os
+import pandas as pd
 from collections import defaultdict
 from numpy import dot
 from numpy.linalg import norm
+
+import cProfile
 
 def createPandasDataFrame(fileName):
     inputFile = os.path.abspath(__file__+"/..")+ "\\" + fileName
     print(f"Reading this file: {inputFile}")
     df = pd.read_csv(inputFile)
     return df
-
+#this function generates four fucntions, this is how people usually translate IDs from raw to inner
+#I explained what is raw and inner ID in later sections.
+def genFourDics(fileName):# this applies to windows, if you are using a linex or Mac, change next line
+    rPath = os.path.abspath(__file__+"/..")+ "\\" + fileName # <---- this is what I mean
+    ratingsPath = rPath
+    uid = []
+    iid = []
+    with open(ratingsPath, newline = '', encoding = 'ISO-8859-1') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader) # pay attention to this row!!!!
+        for row in reader:
+            uid.append(row[0])
+            iid.append(row[1])
+    to_inner_uid = defaultdict()
+    to_inner_iid = defaultdict()
+    to_outer_uid = defaultdict()
+    to_outer_iid = defaultdict()
+    #================== uid ==============================
+    for eachID in uid:
+        if eachID not in to_inner_uid:
+            innerID = len(to_inner_uid)
+            to_outer_uid[innerID] = eachID
+            to_inner_uid[eachID] = innerID
+    #================== iid ==============================
+    for eachID in iid:
+        if eachID not in to_inner_iid:
+            innerID = len(to_inner_iid)
+            #print(innerID)
+            to_outer_iid[innerID] = eachID
+            to_inner_iid[eachID] = innerID  
+            #  
+    return to_inner_uid, to_outer_uid, to_inner_iid, to_outer_iid        
+# This function conferts a dataframe from raw IDs to inner IDs.
+# The inputs are a dataframe of raw IDs and a string name of the data file
+def convertToInner(df_input, fileName):
+    df = df_input
+    to_inner_uid, to_outer_uid, to_inner_iid, to_outer_iid = genFourDics(fileName)
+    #['user_id', 'bus_id', 'rating', 'date','lat','lon','text']
+    #userId,movieId,rating,timestamp
+    uids = df['userId']
+    inner_uid = []
+    for eachOuter in uids:
+        inner_uid.append(to_inner_uid[str(eachOuter)])
+    iids = df['movieId']
+    inner_iid = []
+    for eachOuter in iids:
+        inner_iid.append(to_inner_iid[str(eachOuter)])
+    d = {'userId':inner_uid, 'movieId': inner_iid}
+    df1 = pd.DataFrame(data=d)
+    df['userId'] = df1['userId']
+    df['movieId']  = df1['movieId']
+    return df
+#This function conferts a dataframe from inner IDs to raw IDs.
+#The inputs are same things compared to the above one.
+def convertToOuter(df_input, fileName):
+    df = df_input
+    to_inner_uid, to_outer_uid, to_inner_iid, to_outer_iid = genFourDics(fileName)
+    #['user_id', 'bus_id', 'rating', 'date','lat','lon','text']
+    uids = df['user_id']
+    outer_uid = []
+    for eachInner in uids:
+        if eachInner in to_outer_uid:
+            outer_uid.append(to_outer_uid[eachInner])
+        else:
+            outer_uid.append(eachInner)
+    iids = df['bus_id']
+    print(type(iids))
+    outer_iid = []
+    for eachInner in iids:
+        outer_iid.append(to_outer_iid[eachInner])
+    d = {'user_id':outer_uid, 'bus_id': outer_iid}
+    df1 = pd.DataFrame(data=d)
+    df['user_id'] = df1['user_id']
+    df['bus_id']  = df1['bus_id']
+    return df
 def computeCosine(vec1,vec2):
     return dot(vec1, vec2)/(norm(vec1)*norm(vec2))
-
 def toFloat(aStringSeries):
     aFloatList = []
     if not isinstance(aStringSeries, str):
@@ -38,6 +115,14 @@ def generateBusSimMat(fileName):
         
     return result
 
+def writeToFile(result, writePath):
+
+    with open(writePath,'w', newline = '', encoding = 'ISO-8859-1') as writefile:
+        writer = csv.writer(writePath, sys.stdout, lineterminator = '\n')
+        writer.writerow([ 'bus_id','bus_id','sims'] )
+        for eachBus0 in result:
+            for eachBus1 in result[eachBus0]:
+                writer.writerow([eachBus, result]) 
 
 def extract_s_train(s,trainset):
     s_train = defaultdict()
@@ -54,13 +139,17 @@ def find_POI_neighbors(i, s_train, num_of_neighbors):
     
     
 fileName = "aggregatedVectors.csv"
+writePath = os.path.dirname(os.path.realpath(__file__)) + "\\test.csv"
 
 
-result = generateBusSimMat(fileName)
 
-print(len(result)) 
-for x in result:
-    print(x, result[x][:3], '...')
+
+
+
+#cProfile.run('generateBusSimMat(fileName)')
+#print(len(result)) 
+#for x in result:
+    #print(x, result[x][:3], '...')
     
 
     
