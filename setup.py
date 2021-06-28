9c516d9245b48547ab237c1f51a3202d79abb54c
cuilongyin <cuilongyin@hotmail.com>
2021-06-28 13:45:37 -0400

    Upaded the package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 DESCRIPTION = 'The GBRSs Module Series'
 LONG_DESCRIPTION = 'This package is supposed to allow the running of all GBRS models.'
 
@@ -14,14 +14,13 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=['surprise','pandas','numpy','collections'], # add any additional packages that 
+        install_requires=['surprise','pandas','numpy'], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
         keywords=['python', 'recommender systems', 'privacy preserving'],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Education",
-            "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 3",
             "Operating System :: MacOS :: MacOS X",
             "Operating System :: Microsoft :: Windows",
