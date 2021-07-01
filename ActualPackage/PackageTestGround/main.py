cdf73876eb2d4f9be63c224a8be22bd7da603346
Novák Cui <cuilongyin@hotmail.com>
2021-06-30 21:35:58 -0400

    Package Created

---
+++ ActualPackage/PackageTestGround/main.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../")
+import ActualPackage.models.vanilla as models
+import ActualPackage.functions.methods as functions
+def main():
+    model = models.GBRS_vanilla
+    fileName = "UC.csv"
+    startYear = 2007
+    min_NO_rating = 9999999999   # total is 576065, filtering is too slow because of the matrix being too large.
+    batch_size = 1023     
+    cluster_size = 1      #clusters per batch
+    totalNOB = 33           #number of Batch, not including the test batch
+    factors = 3
+    num_of_centroids = 9
+    functions.totalRun(fileName,model, startYear, min_NO_rating,\
+         totalNOB, cluster_size, batch_size, num_of_centroids, factors )
+
+if __name__ == "__main__":
+    main()
