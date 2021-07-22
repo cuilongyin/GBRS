8f7239936ceae978ed3a37427cb84285c6987a79
cuilongyin <cuilongyin@hotmail.com>
2021-07-22 07:10:40 -0400

    Now we can run two models

--- ActualPackage/PackageTestGround/main.py
+++ ActualPackage/PackageTestGround/main.py
@@ -1,10 +1,12 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../")
-import ActualPackage.models.vanilla as models
+import ActualPackage.models.vanilla as vanila
+import ActualPackage.models.POIsims as POI
 import ActualPackage.functions.methods as functions
+
 def main_vanilla():
-    model = models.GBRS_vanilla
+    model1 = vanila.GBRS_vanilla
     fileName = "UC.csv"
     startYear = 2007
     min_NO_rating = 9999999999   # total is 576065, filtering is too slow because of the matrix being too large.
@@ -14,8 +16,28 @@ def main_vanilla():
     factors = 3
     num_of_centroids = 9
     POIsims = 0
-    functions.totalRun(model, fileName, startYear, min_NO_rating,\
-         totalNOB, cluster_size, batch_size, num_of_centroids, factors,POIsims )
+    functions.totalRun(model, fileName, startYear, min_NO_rating,
+                       totalNOB, cluster_size, batch_size, num_of_centroids, 
+                       factors, POIsims)
+
+def main_POIsims():
+    
+    model = POI.GBRS_POIsims
+    fileName = "UC.csv"
+    startYear = 2007
+    min_NO_rating = 4      # 3: 22785 lines
+    batch_size = 900     
+    cluster_size = 1      #clusters per batch
+    totalNOB = 33           #number of Batch, not including the test batch
+    factors = 3
+    num_of_centroids = 9
+    POIsims = 1
+    functions.totalRun(model, fileName, startYear, min_NO_rating, 
+                       totalNOB, cluster_size, batch_size, num_of_centroids, 
+                       factors, POIsims)
+
+    
 #def main_
 if __name__ == "__main__":
-    main_vanilla()
+    #main_vanilla()
+    main_POIsims()
