32ec5506b0105d36fcac9c174e8acbb44dd86576
cuilongyin <cuilongyin@hotmail.com>
2021-08-12 06:13:13 -0400

    something went wrong
    Former-commit-id: aa7e2deb00a0dc3553763db32dc9468eb427fc7a

--- ActualPackage/models/vanilla.py
+++ ActualPackage/models/vanilla.py
@@ -247,7 +247,7 @@ class GBRS_vanilla(AlgoBase):
                 resultWeight[2] += 1
             elif self.centroidRatingDic[eachCtd][i][1] < 3.3:
                 resultWeight[3] += 1
-            elif self.centroidRatingDic[eachCtd][i][1] < 4.4:
+            elif self.centroidRatingDic[eachCtd][i][1] < 4.5:
                 resultWeight[4] += 1
             else:
                 resultWeight[5] += 1
